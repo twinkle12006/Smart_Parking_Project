15cb6503c72ece54aca690e525e61b2e6a6adec2
twinkle12006 <sirichandanagajula@gmail.com>
2025-12-24 23:17:59 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +11,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Smart_Parking_Project/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
